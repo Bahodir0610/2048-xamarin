9203d606b47ad6ce458db773d5492f96a3c3a862
Bohdan Levantovych <bohdan.levantovych@eleks.com>
2018-05-17 10:24:16 +0300

    first commit

---
+++ App1/App1/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App1
+{
+	public partial class MainPage : ContentPage
+	{
+        static string bomb = new Random().Next(1, 4).ToString();
+
+        public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        async void ButtonClickced(object sender, EventArgs e)
+        {
+            await DisplayAlert("Bomb Exploded", "GAME OVER", "RETRY");
+            Button button = sender as Button;
+            button.Text = new Random().Next(1, 400).ToString();
+        }
+	}
+}
